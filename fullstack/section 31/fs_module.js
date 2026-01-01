f4a1fdd094f0256db3077c2b74dfae4de891a059
rising-dancho <zenlaws02@gmail.com>
2026-01-01 11:00:45 +0800

    feat: learning file system in node

---
+++ fullstack/section 31/fs_module.js
@@ -0,0 +1,24 @@
+// EXAMPLE: ALLOW ARGUMENTS WHEN RUNNING A SCRIPT
+// argv example:
+// const args = process.argv.slice(2)
+// console.log(args)
+
+// for(arg of args){
+//   console.log(`HELLO THERE, ${arg}`)
+// }
+
+// EXAMPLE: CREATE A FOLDER
+const fs = require('fs');
+const folderName = process.argv[2] || 'New Folder1'; // getting the 3rd argument
+
+// asynchronous: fs.mkdir(path[, options], callback)
+// fs.mkdir('D:/tite', (err) => {
+//   if (err) throw err;
+// });
+
+
+// synchronous: fs.mkdirSync(path[, options])
+fs.mkdirSync(`D:/${folderName}`); // creates a folder in the specified location after runnning node fs_module.js [file_name]
+
+// EXAMPLE: CREATE A FILE
+
