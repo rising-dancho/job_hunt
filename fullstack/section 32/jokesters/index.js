814b43c3a012cdee9fc3872f8e32024fc2554b90
rising-dancho <zenlaws02@gmail.com>
2026-01-05 06:46:17 +0800

    feat: working with public api

--- fullstack/section 32/jokesters/index.js
+++ fullstack/section 32/jokesters/index.js
@@ -1,7 +1,9 @@
 // DOCS: https://www.npmjs.com/package/give-me-a-joke#give-me-a-joke
 const jokes = require('give-me-a-joke');
-console.log(jokes)
+const colors = require('colors');
+console.log(jokes);
+console.log(colors);
 
-jokes.getRandomDadJoke(function(joke){
-  console.log(joke)
+jokes.getRandomDadJoke(function (joke) {
+  console.log(joke.rainbow);
 });
