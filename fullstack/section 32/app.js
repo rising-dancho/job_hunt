294321c98717b56f286132f3e505d87fb05e5c46
rising-dancho <zenlaws02@gmail.com>
2026-01-04 21:59:55 +0800

    feat: learning how to require an entire folder or directory

---
+++ fullstack/section 32/app.js
@@ -0,0 +1,2 @@
+const cats = require('./require_entire_folder');
+console.log("REQUIRED AN ENTIRE DIRECTORY!\n",cats);
---
+++ fullstack/section 32/require_entire_folder/index.js
@@ -0,0 +1,15 @@
+const blue = require('./blue');
+const sadie = require('./sadie');
+const janet = require('./janet');
+
+const allCats = [blue, sadie, janet];
+
+// console.log(blue);
+// console.log(sadie);
+// console.log(janet);
+
+// console.log(allCats);
+
+module.exports = allCats;
+
+// WHEN REQUIRING AN ENTIRE FOLDER OR DIRECTORY NODE IS GOING TO LOOK FOR AN INDEX.JS FILE
