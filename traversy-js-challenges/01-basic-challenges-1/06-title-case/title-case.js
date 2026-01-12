fca293549f9b14f131d6b54a9b1d0582578d8e93
rising-dancho <zenlaws02@gmail.com>
2026-01-12 22:08:22 +0800

    feat: logic challenges

--- traversy-js-challenges/01-basic-challenges-1/06-title-case/title-case.js
+++ traversy-js-challenges/01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,23 @@
-function titleCase() {}
+function titleCase(word) {
+  // console.log(word.split(' '));
+  const sentence = word.toLowerCase().split(' ');
+  let result = '';
+
+  for (let i = 0; i < sentence.length; i++) {
+    // console.log(sentence[i][0]); // first letter
+    // console.log(sentence[i].slice(1));
+    let words = sentence[i][0].toUpperCase() + sentence[i].slice(1);
+    // console.log(words);
+    result += words;
+
+    // conditionally add trailing spaces
+    if (i !== sentence.length - 1) {
+      result += ' ';
+    }
+  }
+
+  return result;
+}
+// console.log(titleCase('etits ng kalabaw'));
 
 module.exports = titleCase;
