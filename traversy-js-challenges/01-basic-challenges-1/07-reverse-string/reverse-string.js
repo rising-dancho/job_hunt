2a85fe3d51e1227e19339a418a1ec984e4b9b4cc
rising-dancho <zenlaws02@gmail.com>
2026-01-13 13:01:14 +0800

    feat: logic practice problems

--- traversy-js-challenges/01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ traversy-js-challenges/01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,22 @@
-function reverseString() {}
+// function reverseString(word) {
+//   // console.log(word.split('').reverse().join(''));
+//   return word.split('').reverse().join('');
+// }
 
-module.exports = reverseString;
+// implicit return
+// const reverseString = () => word.split('').reverse().join('');
+
+// console.log(reverseString('world'));
+
+function reverseString(word) {
+  // console.log(word.length);
+  let str = [];
+  for (let i = word.length - 1; i >= 0; i--) {
+    str += word[i];
+  }
+  return str;
+}
+
+console.log(reverseString('world'));
+
+// module.exports = reverseString;
