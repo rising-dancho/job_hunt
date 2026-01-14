f54572e01eaa1e9d283c48cbc1c9135c64b2ce4a
rising-dancho <zenlaws02@gmail.com>
2026-01-14 09:15:39 +0800

    feat: doing logic challenges

--- traversy-js-challenges/01-basic-challenges-1/08-palindrome/palindrome.js
+++ traversy-js-challenges/01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,25 @@
-function isPalindrome() {}
+// if the original word is the same as the reverse of that word its a palindrome
+function isPalindrome(word) {
+  let original = word.toLowerCase().replace(/[^a-z0-9]/g, '');
+  console.log(`original: ${original}`);
+  let reversed = '';
 
-module.exports = isPalindrome;
+  for (let i = original.length - 1; i >= 0; i--) {
+    // console.log(word[i]);
+    reversed += original[i];
+  }
+  console.log(`reversed: ${reversed}`);
+  // console.log(reversed);
+  // return boolean;
+
+  if (original === reversed) {
+    return true;
+  }
+  return false;
+}
+
+console.log(isPalindrome('madam'));
+console.log(isPalindrome('racecar'));
+console.log(isPalindrome('A man, a plan, a canal, Panama'));
+console.log(isPalindrome('12321'));
+// module.exports = isPalindrome;
