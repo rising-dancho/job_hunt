357b3bdff68adbf72bf4f801efd0212460e38a4b
rising-dancho <zenlaws02@gmail.com>
2026-01-14 09:38:53 +0800

    feat: doing logic challenges

--- traversy-js-challenges/01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ traversy-js-challenges/01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,31 @@
-function countVowels() {}
+function countVowels(word) {
+  // function countVowels(str: string): number;
+  let str = '';
+  let count = 0;
+  for (let i = 0; i <= word.length - 1; i++) {
+    // console.log(word[i]);
+    word = word.toLowerCase();
+    str += word[i];
+    if (word[i] === 'a') {
+      count++;
+    }
+    if (word[i] === 'e') {
+      count++;
+    }
+    if (word[i] === 'i') {
+      count++;
+    }
+    if (word[i] === 'o') {
+      count++;
+    }
+    if (word[i] === 'u') {
+      count++;
+    }
+  }
+  console.log(str);
+  console.log(count);
+  return count;
+}
 
+// countVowels('etits');
 module.exports = countVowels;
