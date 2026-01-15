b10c810d1e3c69a042c5ee4b167e6f136599eaab
rising-dancho <zenlaws02@gmail.com>
2026-01-15 11:49:45 +0800

    feat: doing logic challenges

--- traversy-js-challenges/01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ traversy-js-challenges/01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,22 @@
-function removeDuplicates() {}
+/**
+ * Returns a new array with duplicates removed.
+ * @param {any[]} arr - The array to remove duplicates from.
+ * @returns {any[]} - The new array with duplicates removed.
+ */
+function removeDuplicates(array) {
+  let uniqueArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (!uniqueArray.includes(array[i])) {
+      uniqueArray.push(array[i]);
+    }
+  }
+  return uniqueArray;
+}
 
-module.exports = removeDuplicates;
+console.log(removeDuplicates([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+console.log(removeDuplicates([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]));
+console.log(
+  removeDuplicates([1, 2, 3, 4, 5, true, 1, 'hello', 2, 3, 'hello', true])
+);
+
+// module.exports = removeDuplicates;
