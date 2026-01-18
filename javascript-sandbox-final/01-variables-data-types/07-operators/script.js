38a27c2265a00388d5f2b6df793c39b1ec0b4988
rising-dancho <zenlaws02@gmail.com>
2026-01-18 13:42:43 +0800

    feat: learning javascript fundamentals

--- javascript-sandbox-final/01-variables-data-types/07-operators/script.js
+++ javascript-sandbox-final/01-variables-data-types/07-operators/script.js
@@ -1,7 +1,49 @@
 // 1. Arithmetic Operators
+let x;
 
+x = 5 + 5;
+x = 5 - 5;
+x = 5 * 5;
+x = 5 / 5;
+x = 5 % 5; // gives the remainder
+
+// Concatenation
+x = 'rising.' + 'dancho';
+
+// Exponent
+x = 2 ** 2;
+
+// Increment
+x = 1;
+x = x + 1;
+x = x++;
+
+// Decrement
+x = x - 1;
+x = x--;
+
+console.log(x);
 
 // 2. Assignment Operators
-
+x = 10;
+x += 5;
+x -= 5;
+x *= 5;
+x /= 5;
+x **= 5;
+x %= 5;
+console.log(x);
 
 // 3. Comparison Operators
+
+x = 2 == '2';
+x = 2 === '2';
+x = 2 != '2';
+x = 2 !== 2;
+
+x = 10 > 5;
+x = 10 < 5;
+x = 10 >= 5;
+x = 10 <= 5;
+
+console.log(x);
