3cf8272e4c53aa90ffd16b5ffc03d190c247ba35
rising-dancho <zenlaws02@gmail.com>
2026-01-18 21:39:35 +0800

    feat: learning javascript fundamentals

--- javascript-sandbox-final/01-variables-data-types/10-capitalize-challenge/script.js
+++ javascript-sandbox-final/01-variables-data-types/10-capitalize-challenge/script.js
@@ -1,2 +1,11 @@
 // Create a new string called "myNewString" that holds the value of "Developer", using the value from "myString"
 const myString = 'developer';
+
+let initial = myString[0].toUpperCase();
+let restOfTheWord = myString.slice(1);
+
+console.log(initial + restOfTheWord);
+
+let position_0 = myString.charAt(0).toUpperCase();
+
+console.log(position_0 + restOfTheWord);
