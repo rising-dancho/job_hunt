26a36a9b6ef52f37abe01e643b4947807f5cc8a4
rising-dancho <zenlaws02@gmail.com>
2026-01-29 11:06:19 +0800

    feat: typescript

--- typescript/app.js
+++ typescript/app.js
@@ -1,5 +1,21 @@
+var num1Input = document.getElementById('num1');
+var num2Input = document.getElementById('num2');
+var buttonElement = document.querySelector('button');
 function add(a, b) {
     return a + b;
 }
-var result = add(5, 6);
-console.log(result);
+// concersions: .toString, .parseInt
+// void means: doesnt have a return statement
+function printResult(result) {
+    console.log(result);
+}
+// TYPE INFERENCE: no need to re-assign a type if it is infered by typescript
+// const result: number = add(5, 6); ❌ : no need to assign a type to result variable
+// const result: number = add(5, 6);
+// console.log(result);
+buttonElement === null || buttonElement === void 0 ? void 0 : buttonElement.addEventListener('click', function () {
+    var num1 = +num1Input.value;
+    var num2 = +num2Input.value;
+    var result = add(num1, num2);
+    printResult(result);
+});
--- typescript/app.ts
+++ typescript/app.ts
@@ -1,6 +1,25 @@
+const num1Input = document.getElementById('num1') as HTMLInputElement;
+const num2Input = <HTMLInputElement>document.getElementById('num2');
+const buttonElement = document.querySelector('button') as HTMLButtonElement;
+
 function add(a: number, b: number) {
   return a + b;
 }
+// concersions: .toString, .parseInt
+// void means: doesnt have a return statement
 
-const result: number = add(5, 6);
-console.log(result);
+function printResult(result: number): void {
+  console.log(result);
+}
+
+// TYPE INFERENCE: no need to re-assign a type if it is infered by typescript
+// const result: number = add(5, 6); ❌ : no need to assign a type to result variable
+// const result: number = add(5, 6);
+// console.log(result);
+
+buttonElement?.addEventListener('click', () => {
+  const num1 = +num1Input.value;
+  const num2 = +num2Input.value;
+  const result = add(num1, num2);
+  printResult(result);
+});
